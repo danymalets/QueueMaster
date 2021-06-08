9c7df7bcc6044c53ce8ee5dab2d9f9e9315b4ca7
danymalets <malets.d.l@gmail.com>
2021-06-08 17:56:26 +0300

    docker public image

--- queue_master/settings.py
+++ queue_master/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config.DEBUG
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
