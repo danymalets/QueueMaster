41644960d05c1d097d792bf636dd2877cc2ec03b
danymalets <malets.d.l@gmail.com>
2021-06-11 20:32:48 +0300

    fix db

--- webserver/queue_master/settings.py
+++ webserver/queue_master/settings.py
@@ -74,15 +74,15 @@ DATABASES = {
         'PORT': 5432,
     }
 }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 #
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
